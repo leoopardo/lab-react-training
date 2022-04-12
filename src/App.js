97158f67a1f3bcde60ed7126af2ba72a51eeab32
Leonardo Araujo <leo.san9@hotmail.com>
2022-04-12 20:30:08 -0300

    parte 2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import "./App.css"
 import {IdCard} from "./assets/components/idCard/idCard"
 import {Greetings} from "../src/assets/components/greetings/greetings"
+
 function App() {
   return (
     <div className="App">
-      <IdCard
-        lastName='Doe'
-        firstName='John'
-        gender='male'
-        height={178}
-        birth={new Date("1992-07-14")}
-        picture="https://randomuser.me/api/portraits/men/44.jpg"
-      />
+      <div className="labOne">
+        <IdCard
+          lastName='Doe'
+          firstName='John'
+          gender='male'
+          height={178}
+          birth={new Date("1992-07-14")}
+          picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
 
-      <IdCard
-        lastName='Delores '
-        firstName='Obrien'
-        gender='female'
-        height={172}
-        birth={new Date("1988-05-11")}
-        picture="https://randomuser.me/api/portraits/women/44.jpg"
-      />
-      <Greetings lang="de">Ludwig</Greetings>
-      <Greetings lang="fr">François</Greetings>
+        <IdCard
+          lastName='Delores '
+          firstName='Obrien'
+          gender='female'
+          height={172}
+          birth={new Date("1988-05-11")}
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+      </div>
+      <div className="labTwo">
+        <Greetings lang="de">Ludwig</Greetings>
+        <Greetings lang="fr">François</Greetings>
+      </div>
     </div>
   );
 }
--- src/assets/components/greetings/greetings.js
+++ src/assets/components/greetings/greetings.js
@@ -1,9 +1,21 @@
 import "./style-greetings.css"
 
 export function Greetings(props){
-    return (
-        <div>
+    let greet = ""
+    
+    if(props.lang === "de"){
+        greet = "Hallo"
+    } else if(props.lang === "en"){
+        greet = "Hello"
+    } else if(props.lang === "es"){
+        greet = "Holla"
+    } else if(props.lang === "fr"){
+        greet = "Bonjour"
+    };
 
+    return (
+        <div className="grt">
+        <p>{greet}, {props.children}!</p>
         </div>
     )
 }
